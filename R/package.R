6bb4151259170387e00b48bab3697276463d1e55
Rainer M. Krug <R.M.Krug@gmail.com>
2013-10-01 10:19:52 +0200

    Change to Imports
    - Change to Imports to from Depends
    - Release version 0.5.0

--- R/package.R
+++ R/package.R
@@ -3,11 +3,11 @@
 ##' This package is a collection of different seed sispersal routines. 
 ##'
 ##' 
-##' ##' \tabular{ll}{
+##' \tabular{ll}{
 ##' Package: \tab seedDisp\cr
 ##' Type: \tab Package\cr
-##' Version: \tab MAJORVERSION.MINORVERSION-PATCHVERSION\cr
-##' Date: \tab TODAYSDATE\cr
+##' Version: \tab 0.5-0\cr
+##' Date: \tab 2013-10-01_10-14\cr
 ##' License: \tab GPL (>= 2)\cr
 ##' LazyLoad: \tab yes\cr
 ##' }
@@ -15,6 +15,9 @@
 ##' @name seedDisp-package
 ##' @docType package
 ##' @author Rainer M Krug \email{Rainer@@krugs.de}
+##' @import Rcpp
+##' @import sp
+##' @import spgrass6
 NULL
 
 ## Local Variables:
